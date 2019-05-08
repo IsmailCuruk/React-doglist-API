0ba4e4adaf851ca94f9624659cd6581565ec5c73
IsmailCuruk <ismailcrk@gmail.com>
2019-05-08 14:22:38 +0200

    Working (before splitting it to containers)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import DogsList from './components/DogsList'
+import request from 'superagent'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <h1>
+          Exercise Week 3/ Day 3:
+        </h1>
+        </header>
+        <main>
+          <DogsList />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
